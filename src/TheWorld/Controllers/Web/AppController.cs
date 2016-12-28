ce77bb84378fdd3c14f5eeeb73b6fab019a63a10
Srinivasu Chattu <chsrinivas289@gmail.com>
2016-12-28 16:45:07 -0500

    Adding the all files.

---
+++ src/TheWorld/Controllers/Web/AppController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using TheWorld.Services;
+using TheWorld.ViewModels;
+
+namespace TheWorld.Controllers.Web
+{
+    public class AppController : Controller
+    {
+        private iMailService _mailService;
+        private IConfigurationRoot _config;
+
+        public AppController(iMailService mailService, IConfigurationRoot config)
+        {
+            _mailService = mailService;
+            _config = config;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Contact(ContactViewModel contact)
+        {
+            if (contact.Email.Contains("@iol.com"))
+            {
+                ModelState.AddModelError("Email", "We wont support iol email addresses.");
+            }
+            if(ModelState.IsValid)
+            {
+                _mailService.SendMail(_config["MailSettings:ToAddress"], "ASPNETCORE@microsoft.com", "Subject of the Mail", "Body of the Mail");
+                ModelState.Clear();
+                ViewBag.Message = "Message Sent";
+            }
+            return View();
+        }
+
+        public JsonResult getAPI()
+        {
+            var result = new { Name = "API", Version = "1.0.0", Technology = "APSNETCORE" };
+            JsonResult json = new JsonResult(result);
+            return json;
+        } 
+    }
+}
--- src/TheWorld/Startup.cs
+++ src/TheWorld/Startup.cs
@@ -7,15 +7,32 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using TheWorld.Services;
+using Microsoft.Extensions.Configuration;
 
 namespace TheWorld
 {
     public class Startup
     {
+        private IHostingEnvironment _env;
+        private IConfigurationRoot _config;
+
+        public Startup(IHostingEnvironment env)
+        {
+            _env = env;
+            var builder =  new ConfigurationBuilder()
+                .AddJsonFile("Config.json")
+                .SetBasePath(_env.ContentRootPath);
+            _config = builder.Build();
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSingleton(_config);
+            services.AddScoped<iMailService, DebugMailService>();
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -28,9 +45,18 @@ namespace TheWorld
                 app.UseDeveloperExceptionPage();
             }
 
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
+            //app.Run(async (context) =>
+            //{
+            //    await context.Response.WriteAsync("<HTML><body>Hello World!</body></HTML>");
+            //});
+            //app.UseDefaultFiles();
+            app.UseStaticFiles();
+
+            app.UseMvc(config => {
+                config.MapRoute(
+                    name: "Default",
+                    template: "{Controller}/{action}/{id?}",
+                    defaults: new { Controller = "App", action = "Index" });
             });
         }
     }
